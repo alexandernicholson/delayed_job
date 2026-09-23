c7f1ebdbfbe38597259b3112a68a63c45def9e05
DragonStuff <4584443+DragonStuff@users.noreply.github.com>
2026-09-23 01:58:50 -0500

    chore(test): drop queue schema workaround

--- test/support/ops_queue_schema.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-if BACKEND == :active_record && !SolidQueue::Record.connection_pool.with_connection { |connection| connection.table_exists?(:solid_queue_jobs) }
-  schema = File.read(File.join(Gem.loaded_specs["solid_queue"].full_gem_path, "lib/generators/solid_queue/install/templates/db/queue_schema.rb"))
-  body = schema.sub(/\AActiveRecord::Schema\[[\d.]+\]\.define\(version: \d+\) do\n/, "").sub(/end\s*\z/, "")
-  SolidQueue::Record.connection_pool.with_connection { |connection| connection.instance_eval(body) }
-  SolidQueue::Record.reset_column_information
-  SolidQueue::Record.descendants.each(&:reset_column_information)
-end
